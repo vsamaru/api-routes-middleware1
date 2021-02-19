7a0867620148caba5c7f4e220f6527d79fdfaae2
vsamaru <vsamaru@gmail.com>
2021-02-19 23:09:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/api/x', // Matched parameters can be used in the destination
+        source: '/(.*)',
+        destination: '/api/x?o=$1', // Matched parameters can be used in the destination
         permanent: true,
       },
     ]
