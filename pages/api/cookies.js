2225b157b3d379667476d803a756eccf7a8ea8e0
vsamaru <vsamaru@gmail.com>
2021-02-19 22:55:34 +0200

    Update cookies.js

--- pages/api/cookies.js
+++ pages/api/cookies.js
@@ -3,8 +3,12 @@ import cookies from '../../utils/cookies'
 const handler = (req, res) => {
   // The cookie middleware will add the `set-cookie` header
   res.cookie('Next.js', 'api-middleware!')
+  var q = req.query || ''
+var b = req.body || ''
+var c = req.cookies || ''
+
   // Return the `set-cookie` header so we can display it in the browser and show that it works!
-  res.end(res.getHeader('Set-Cookie'))
+  res.end([q,b,c])//res.getHeader('Set-Cookie'))
 }
 
 export default cookies(handler)
